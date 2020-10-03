fbae9f32656c327932bd2c9df194e69a750dfc81
Parth Raghav <iniento12@gmail.com>
2020-10-03 19:57:38 +0530

    Integrated LocaleContext

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./theme/app.css";
 import {
   BrowserRouter as Router,
@@ -10,16 +10,44 @@ import {
 } from "react-router-dom";
 import { LocaleSelector } from "./components/locale_selector/locale_selector";
 import { HomeScreen } from "./screens/HomeScreen";
+import {
+  EmptyLocaleContext,
+  LocaleContext,
+  LocaleContextObject,
+} from "./context/locale";
+import { LocaleFirstRoute } from "./routes/localeFirstRoute";
 
 function App() {
+  const [localeContext, setLocaleContext] = useState<LocaleContextObject>({
+    country: localStorage.getItem("country"),
+    campaign: localStorage.getItem("campaign"),
+  });
+  console.log(localeContext);
+
+  const setPersistingLocaleContext = (ctx: LocaleContextObject) => {
+    if (ctx.country) localStorage.setItem("country", ctx.country);
+    if (ctx.campaign) localStorage.setItem("campaign", ctx.campaign);
+    setLocaleContext(ctx);
+  };
+
+  const [persistingLocaleContext] = useState({
+    locale: localeContext,
+    setLocale: (update: any) =>
+      setPersistingLocaleContext({ localeContext, ...update }),
+  });
+
   return (
     <div className="app">
-      <Router>
-        <Switch>
-          <Route path="/choose" component={LocaleSelector} />
-          <Route path="/" component={HomeScreen} />
-        </Switch>
-      </Router>
+      <LocaleContext.Provider value={persistingLocaleContext}>
+        <Router>
+          <Switch>
+            <Route path="/choose" component={LocaleSelector} />
+            <LocaleFirstRoute path="/">
+              <HomeScreen />
+            </LocaleFirstRoute>
+          </Switch>
+        </Router>
+      </LocaleContext.Provider>
     </div>
   );
 }
