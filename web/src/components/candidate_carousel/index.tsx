09abd7d443c895dfd83b1edad4da155badbab94b
Parth Raghav <iniento12@gmail.com>
2020-10-04 00:05:04 +0530

    Fixed a markup issue

--- web/src/components/candidate_carousel/index.tsx
+++ web/src/components/candidate_carousel/index.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import "./styles.css";
 
 export const CandidateCarousel = ({ data }: any) => {
+  console.log(data);
   return (
-    <div className="candidate-carousel">
-      {data.map((candidate: any, index: number) => (
-        <div key={index} className="carousel-el-container">
-          <img src={candidate.imageLink} className="carousel-el-img" />
-          <div className="carousel-el-info-container">
-            <span className="candidate-name">{candidate.name}</span>
-            <span className="candidate-party">{candidate.party}</span>
+    <div className="candidate-carousel-container">
+      <div className="candidate-carousel">
+        {data.map((candidate: any, index: number) => (
+          <div key={index} className="carousel-el-container">
+            <img src={candidate.imageLink} className="carousel-el-img" />
+            <div className="carousel-el-info-container">
+              <span className="candidate-name">{candidate.name}</span>
+              <span className="candidate-party">{candidate.party}</span>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
