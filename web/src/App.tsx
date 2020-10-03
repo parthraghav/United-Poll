354cf49ec1740e1f72d22173a7d54c529839b49d
Parth Raghav <iniento12@gmail.com>
2020-10-03 18:34:34 +0530

    Added router to app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,25 @@
 import React from "react";
-import "./App.css";
+import "./theme/app.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+import { LocaleSelector } from "./components/locale_selector/locale_selector";
+import { HomeScreen } from "./screens/HomeScreen";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/choose" component={LocaleSelector} />
+          <Route path="/" component={HomeScreen} />
+        </Switch>
+      </Router>
     </div>
   );
 }
