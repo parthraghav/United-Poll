efbecca713b9adbba956eb91f4f1975478aec3b6
Parth Raghav <iniento12@gmail.com>
2020-10-04 00:07:44 +0530

    Added dummy data

---
+++ web/src/dummy_data.ts
@@ -0,0 +1,120 @@
+export const dummyCandidateData = [
+  {
+    name: "Abby Broyles",
+    party: "Democratic Party",
+    imageLink:
+      "https://firebasestorage.googleapis.com/v0/b/unitedpoll-71c75.appspot.com/o/candidate_display_pictures%2F9da02184ae44850a70f24b4428c37aed.png?alt=media",
+  },
+  {
+    name: "Elysabeth Britt",
+    party: "Democratic Party",
+    imageLink:
+      "https://firebasestorage.googleapis.com/v0/b/unitedpoll-71c75.appspot.com/o/candidate_display_pictures%2F9e454dfe12ebe22de6205c86c9ff7584.png?alt=media",
+  },
+  {
+    name: "Gary Johnson",
+    party: "Independent Candidate",
+    imageLink:
+      "https://firebasestorage.googleapis.com/v0/b/unitedpoll-71c75.appspot.com/o/candidate_display_pictures%2F0439a5ea7bc5b4c3796de9bef212565a.png?alt=media",
+  },
+  {
+    name: "Jim Inhofe",
+    party: "Republican Party",
+    imageLink:
+      "https://firebasestorage.googleapis.com/v0/b/unitedpoll-71c75.appspot.com/o/candidate_display_pictures%2F2117bcf76424bd29c8cbfbc167a83b30.png?alt=media",
+  },
+  {
+    name: "JJ Stitt",
+    party: "Republican Party",
+    imageLink:
+      "https://firebasestorage.googleapis.com/v0/b/unitedpoll-71c75.appspot.com/o/candidate_display_pictures%2F86a4c42168fcd10097d1ed46b30436e9.png?alt=media",
+  },
+  {
+    name: "Jim Inhofe",
+    party: "Republican Party",
+    imageLink:
+      "https://firebasestorage.googleapis.com/v0/b/unitedpoll-71c75.appspot.com/o/candidate_display_pictures%2F9da02184ae44850a70f24b4428c37aed.png?alt=media",
+  },
+];
+export const dummyQAData = [
+  {
+    name: "What will you do to secure healthcare for american citizens?",
+    views: 3500,
+    shares: 289,
+    answer_count: 5,
+  },
+  {
+    name: "Do you think the United States is a racist country?",
+    views: 3500,
+    shares: 289,
+    answer_count: 11,
+  },
+  {
+    name:
+      "Do you think the next administration will need to investigate President Trump or members of his family?",
+    views: 3500,
+    shares: 289,
+    answer_count: 5,
+  },
+  {
+    name:
+      "What is the most urgent economic challenge facing the United States today?",
+    views: 3500,
+    shares: 289,
+    answer_count: 5,
+  },
+  {
+    name:
+      "How will you tackle the issues of police brutality and racism in the criminal justice system?",
+    views: 3500,
+    shares: 289,
+    answer_count: 5,
+  },
+  {
+    name:
+      "How will you engage with government scientists, public health experts, and researchers, especially in ongoing efforts to address the pandemic?",
+    views: 3500,
+    shares: 289,
+    answer_count: 5,
+  },
+  {
+    name: "How will you address the recent surge in voter suppression efforts?",
+    views: 3500,
+    shares: 289,
+    answer_count: 5,
+  },
+  {
+    name:
+      "How will you tackle the influence in Washington of special interest groups such as the fossil fuel lobby?",
+    views: 3500,
+    shares: 289,
+    answer_count: 5,
+  },
+  {
+    name:
+      "How will you work with lawmakers to prioritize ethics reforms in Washington?",
+    views: 3500,
+    shares: 289,
+    answer_count: 5,
+  },
+  {
+    name:
+      "What will you do to address white nationalism and the resurgence of hate crimes?",
+    views: 3500,
+    shares: 289,
+    answer_count: 5,
+  },
+  {
+    name:
+      " If elected, what Senate procedural reforms would you propose? Do you think the Senate should eliminate the filibuster?",
+    views: 3500,
+    shares: 289,
+    answer_count: 5,
+  },
+  {
+    name: "What will you do to secure healthcare for american citizens?",
+    views: 3500,
+    shares: 289,
+    answer_count: 5,
+  },
+];
