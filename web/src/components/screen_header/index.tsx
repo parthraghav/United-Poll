68a9265f53ac4bc2eb73c0019fc89edc1814a16e
Parth Raghav <iniento12@gmail.com>
2020-10-03 21:49:36 +0530

    Added ScreenHeader component

--- web/src/components/screen_header/index.tsx
+++ web/src/components/screen_header/index.tsx
@@ -3,18 +3,38 @@ import { LocaleContext } from "../../context/locale";
 import "./styles.css";
 // @ts-ignore
 import ReactCountryFlag from "react-country-flag";
+import { useHistory, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
 export const ScreenHeader = () => {
   const { locale } = useContext(LocaleContext);
+  const history = useHistory();
+  const location = useLocation();
+  console.log(location);
+  const handleLocaleClick = () => {
+    history.push("/choose");
+  };
+  const handleBackBtnClick = () => {
+    history.goBack();
+  };
   return (
     <div className="screen-header">
       <div>
+        {location.pathname != "/" && (
+          <div onClick={handleBackBtnClick}>
+            <FontAwesomeIcon icon={faChevronLeft} />
+            <span className="back-btn-label">Back</span>
+          </div>
+        )}
+      </div>
+      <div onClick={handleLocaleClick}>
         <ReactCountryFlag
           countryCode={locale.country}
           className="locale-selector-country-flag"
           style={{ fontSize: "2em" }}
         />
-        <span>{locale.campaign}</span>
+        <span className="locale-campaign-name">{locale.campaign}</span>
       </div>
     </div>
   );
