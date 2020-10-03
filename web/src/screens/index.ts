317af61efedabb5a5a5b9d095178eb8d69e5004d
Parth Raghav <iniento12@gmail.com>
2020-10-04 00:07:38 +0530

    Refactor

---
+++ web/src/screens/index.ts
@@ -0,0 +1,5 @@
+import { HomeScreen } from "./home_screen";
+import { ProfileScreen } from "./profile_screen";
+import { DebateScreen } from "./debate_screen";
+
+export { HomeScreen, ProfileScreen, DebateScreen };
