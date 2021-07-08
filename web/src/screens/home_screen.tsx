ecd836938936165683da6e23935d65b162b07828
Parth Raghav <iniento12@gmail.com>
2021-07-08 20:36:23 +0530

    integrated home screen

--- web/src/screens/home_screen.tsx
+++ web/src/screens/home_screen.tsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CandidateCarousel, QAGrid, Screen, ScreenHeader } from "../components";
+import { getActivePoliticians, Politician } from "../core/politician";
+import { getTopQuestions } from "../core/question";
 import { dummyCandidateData, dummyQAData } from "../dummy_data";
 import "./home_screen.css";
 
 export const HomeScreen = (props: any) => {
+  const [activePoliticians, setActivePoliticians] = useState<Array<Politician>>(
+    []
+  );
+  const [topQuestions, setTopQuestions] = useState<Array<any>>([]);
+  useEffect(() => {
+    (async () => {
+      const _politicians = await getActivePoliticians();
+      setActivePoliticians(_politicians);
+    })();
+
+    (async () => {
+      const _questions = await getTopQuestions();
+      setTopQuestions(_questions);
+      console.log(_questions);
+    })();
+  }, []);
   return (
     <Screen>
       <ScreenHeader />
       <div className="padded-block home-screen-info-bar">
-        <h1>148 Candidates in running</h1>
+        <h1>Candidates in running</h1>
         <p>
           List is inclusive of Republican Party, Democratic Party and
           Independent Candidates
         </p>
       </div>
-      <CandidateCarousel data={dummyCandidateData} />
+      <CandidateCarousel data={activePoliticians} />
       <div className="padded-block home-screen-info-bar">
-        <h1>Top 300 Questions</h1>
+        <h1>Top Questions</h1>
         <p>Answered by your candidates. Compare them, annotate them!</p>
       </div>
-      <QAGrid data={dummyQAData} />
+      <QAGrid data={topQuestions} />
     </Screen>
   );
 };
--- web/src/screens/index.ts
+++ web/src/screens/index.ts
@@ -1,6 +1,6 @@
-import { HomeScreen } from "./home_screen";
-import { ProfileScreen } from "./profile_screen";
-import { DebateScreen } from "./debate_screen";
-import { AddScreen } from "./add_screen";
-
-export { HomeScreen, ProfileScreen, DebateScreen, AddScreen };
+export { HomeScreen } from "./home_screen";
+export { ProfileScreen } from "./profile_screen";
+export { DebateScreen } from "./debate_screen";
+export { AddScreen } from "./add_screen";
+export { LoadingScreen } from "./loading_screen";
+export { WelcomeScreen } from "./welcome_screen";
