4260bc9b2e7d73e88510616865cc1b2252e83db3
Parth Raghav <iniento12@gmail.com>
2021-07-08 20:31:24 +0530

    refactored components

--- web/src/components/secondary_button.tsx
+++ web/src/components/secondary_button.tsx
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-export const PrimaryButton = ({ label, icon, ...rest }: any) => {
+export const SecondaryButton = ({ label, icon, ...rest }: any) => {
   return (
-    <div {...rest} className="primary-button">
-      <span>{label}</span>
+    <div {...rest} className="secondary-button">
       {icon && <FontAwesomeIcon icon={icon} />}
+      <span className="back-btn-label">{label}</span>
     </div>
   );
 };
