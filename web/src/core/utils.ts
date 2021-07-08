5884a3ce0c936c84b1417438f6c4841cc035dfc5
Parth Raghav <iniento12@gmail.com>
2021-07-08 20:34:46 +0530

    added utility methods

--- web/src/core/utils.ts
+++ web/src/core/utils.ts
@@ -6,3 +6,25 @@ export const retrieveVideoId = (url: string) => {
   }
   return video_id;
 };
+
+export const validateEmail = (email: string) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+export const computeDaysUntil = (futureDate: Date) => {
+  var d = 24 * 60 * 60 * 1000;
+  return Math.round(
+    Math.abs((new Date().getTime() - futureDate.getTime()) / d)
+  );
+};
+
+export const isValidYoutubeUrl = (url: string) => {
+  var p =
+    /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+  if (url.match(p)) {
+    return url.match(p)![1];
+  }
+  return false;
+};
